930e1244a82cc151b0ddc497dda7c8168991d6d2
matheusgzamboni <matheusgzamboni@gmail.com>
2022-01-26 17:24:13 -0300

    add newUser post method to user.routes

--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -3,6 +3,6 @@ const usersRoutes = express.Router()
 const userController = require('../../controllers/userController')
 
 usersRoutes.get('/users', userController.index)
-usersRoutes.post('/user', userController.addNewUser)
+usersRoutes.post('/user', userController.newUser)
 
 module.exports = usersRoutes
