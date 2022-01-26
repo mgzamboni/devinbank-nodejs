523443ad0f70f43d166e72dbe1dab919534e2c16
matheusgzamboni <matheusgzamboni@gmail.com>
2022-01-26 17:27:55 -0300

    add auxiliary methods to manage data insertion

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -9,14 +9,29 @@ function getData(filename){
     return result
 }
 
-function insertData(filename, data){
+function insertData(filename, data, ...keys){
     let result = JSON.parse(fileSystem.readFileSync('src/database/'+filename, 'utf8'));
     if (Array.isArray(result)){
-        result.push(data)
-        fileSystem.writeFileSync('src/database/'+filename, JSON.stringify(result))
-        return result.length
+        DuplicateObj = checkDuplicateObj(result, data, keys)
+        if(!DuplicateObj) {
+            result.push(data)
+            fileSystem.writeFileSync('src/database/'+filename, JSON.stringify(result))
+            return result.length
+        } else {
+            return getDuplicateKey(DuplicateObj, data, keys)
+        }
     } else {
         return null
+    }
+}
+
+function checkDuplicateObj(objArray, data, keys) {
+    return objArray.find(obj => keys.find(key => { if (obj[key] === data[key]) return key}) )
+}
+
+function getDuplicateKey(obj, data, keys) {
+    if(obj !== undefined) {
+        return keys.filter(key => { return obj[key] === data[key]})
     }
 }
 
