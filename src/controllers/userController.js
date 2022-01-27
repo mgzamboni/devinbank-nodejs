be3e8ed137b21c1efa5b785b097597f2c27609e4
matheusgzamboni <matheusgzamboni@gmail.com>
2022-01-27 01:33:30 -0300

    update addNewUser method with all validations

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-const { getData, insertData, createOrUpdateData, getDataArray, validateDB } = require('../utils/functions')
+const { getData, insertData, createOrUpdateData, validateDB, validateInput } = require('../utils/functions')
 const userService = require('../services/user.service')
 
 module.exports = {
@@ -8,19 +8,27 @@ module.exports = {
         return res.status(200).json({users: users})
     },
 
-    async addNewUser(req, res){
+    async addNewUser(req, res) {
         if (validateDB('users.json') === false) return res.status(500).json({message: 'users.json database not found'})
-        const userData = req.body
+
+        const checkInput = validateInput(req.body)
+        if(checkInput.length)
+            return res.status(400).json({message: checkInput})
+
+        const { id, name, email } = req.body;
+        const userData = {
+            id: id,
+            name: name,
+            email: email
+        }
+
         insertStatus = insertData('users.json', userData, 'id', 'email')
-        console.log(insertStatus)
-        if(!insertStatus) {
+
+        if(!insertStatus) 
             return res.status(500).json({message: 'user.json database corrupted'})
-        }
-        else if(Array.isArray(insertStatus)) {
+        else if(Array.isArray(insertStatus)) 
             return res.status(400).json({message: insertStatus.map(dataKey => `The ${dataKey} '${userData[dataKey]}' is being used`)})
-        }
-        else    {
+        else 
             return res.status(200).json(getData('users.json'))
-        }
     },
 }
